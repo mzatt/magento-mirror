c686148706b28d154636e38fa53128cb263720c6
mzatt <matt.andreo@palmettostatearmory.com>
2018-09-24 11:16:28 -0400

    param store

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,1 +1,8 @@
-aws s3 cp s3://$CONFIG_BUCKET/stage/ /var/www/html/ --recursive
+#!/bin/bash
+#Script to pull bucketname from Parameter store, download configurations into 
+config=$(aws ssm get-parameters --region us-east-2 --names CONFIG_BUCKET --query Parameters[0].Value)
+config=`echo $config | sed -e 's/^"//' -e 's/"$//'`
+aws s3 cp s3://$config/stage/ /var/www/html/ --recursive
+
+#run setup scripts
+#flush cache
